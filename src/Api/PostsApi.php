7d9a6814b931204adf5835ec9cc38846cda07e14
Sevak Mnatcakanian <sevachello@gmail.com>
2025-09-23 22:26:35 +0700

    Posts api endpoints

--- src/Api/PostsApi.php
+++ src/Api/PostsApi.php
@@ -3,6 +3,7 @@
 namespace Sevaske\Discourse\Api;
 
 use Sevaske\Discourse\Contracts\DiscourseResponseContract;
+use Sevaske\Discourse\Exceptions\DiscourseException;
 
 class PostsApi extends ApiService
 {
@@ -16,6 +17,29 @@ class PostsApi extends ApiService
     public function get($id): DiscourseResponseContract
     {
         return $this->request('GET', "/posts/{$id}.json");
+    }
+
+    public function update($id, string $raw, ?string $editReason = null): DiscourseResponseContract
+    {
+        return $this->request('PUT', "/posts/{$id}.json", [
+            'post' => $this->filterData([
+                'raw' => $raw,
+                'edit_reason' => $editReason,
+            ]),
+        ]);
+    }
+
+    /**
+     * @api https://docs.discourse.org/#tag/Posts/operation/createTopicPostPM
+     *
+     * @param array $data
+     *
+     * @return DiscourseResponseContract
+     * @throws DiscourseException
+     */
+    public function create(array $data): DiscourseResponseContract
+    {
+        return $this->request('POST', '/posts.json', $data);
     }
 
     public function delete($id): DiscourseResponseContract
@@ -38,5 +62,19 @@ class PostsApi extends ApiService
     public function unlock($id): DiscourseResponseContract
     {
         return $this->locked($id, false);
+    }
+
+    public function replies($id): DiscourseResponseContract
+    {
+        return $this->request('GET', "/posts/{$id}/replies.json");
+    }
+
+    public function action(int $postId, int $postActionTypeId, ?bool $flagTopic = null): DiscourseResponseContract
+    {
+        return $this->request('GET', '/post_actions.json', [
+            'id' => $postId,
+            'post_action_type_id' => $postActionTypeId,
+            'flag_topic' => $flagTopic,
+        ]);
     }
 }
